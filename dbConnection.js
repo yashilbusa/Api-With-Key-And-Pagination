675091f24f30234079f01548462377c0762c63cb
yashilbusa <yashilpatel1914@gmail.com>
2025-02-27 14:19:23 +0530

    commit

---
+++ dbConnection.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const db = mongoose.connect(process.env.url)
+.then(() => {
+    console.log('Connected to MongoDB');
+    console.log(`Server is Running on http://localhost:${process.env.port}`)
+})
+.catch((err) => {
+    console.error(`Error connecting to MongoDB : ${err}`);
+});
+
+export default db
