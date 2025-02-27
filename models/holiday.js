c8d137fe3bedd812c8564840e69184937fed093e
yashilbusa <yashilpatel1914@gmail.com>
2025-02-27 16:58:50 +0530

    commit

--- models/holiday.js
+++ models/holiday.js
@@ -1,31 +1,39 @@
-import mongoose from 'mongoose';
+import mongoose, { Mongoose } from 'mongoose';
+
+const countrySchema = new mongoose.Schema({
+    id:String,
+    name:String
+})
+
+const dateTimeSchema = new mongoose.Schema({
+    year:Number,
+    month:Number,
+    day:Number,
+    year:Number,
+    minute:Number,
+    second:Number
+})
+
+const timezoneSchema = new mongoose.Schema({
+    offset:String,
+    zoneabb:String,
+    zoneoffset:Number,
+    zonedst:Number,
+    zonetotaloffset:Number
+})
+
+const dateSchema = new mongoose.Schema({
+    iso:String,
+    datetime:{type:dateTimeSchema},
+    timezone:{type:timezoneSchema}
+})
 
 const holidayScehma = new mongoose.Schema({
     name:String,
     description:String,
-    country:{
-        id:String,
-        name:String
-    },
-    date:{
-        iso:String,
-        datetime:{
-            year:Number,
-            month:Number,
-            day:Number,
-            year:Number,
-            minute:Number,
-            second:Number
-        },
-        timezone:{
-            offset:String,
-            zoneabb:String,
-            zoneoffset:Number,
-            zonedst:Number,
-            zonetotaloffset:Number
-        }
-    },
-    type:[],
+    country:{ type: countrySchema },
+    date:{ type:dateSchema },
+    type:[String],
     primary_type:String,
     canonical_url:String,
     urlid:String,
