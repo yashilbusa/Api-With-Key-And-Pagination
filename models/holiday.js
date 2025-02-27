651ffdb6a16f2547086668a706a3fbb24b0775e6
yashilbusa <yashilpatel1914@gmail.com>
2025-02-27 21:55:06 +0530

    commit

--- models/holiday.js
+++ models/holiday.js
@@ -1,45 +1,45 @@
-import mongoose, { Mongoose } from 'mongoose';
+import mongoose from 'mongoose';
 
 const countrySchema = new mongoose.Schema({
-    id:String,
-    name:String
-})
-
-const dateTimeSchema = new mongoose.Schema({
-    year:Number,
-    month:Number,
-    day:Number,
-    minute:Number,
-    second:Number
-})
-
-const timezoneSchema = new mongoose.Schema({
-    offset:String,
-    zoneabb:String,
-    zoneoffset:Number,
-    zonedst:Number,
-    zonetotaloffset:Number
-})
-
-const dateSchema = new mongoose.Schema({
-    iso:String,
-    datetime:{type:dateTimeSchema},
-    timezone:{type:timezoneSchema}
-})
-
-const holidayScehma = new mongoose.Schema({
-    name:String,
-    description:String,
-    country:{ type: countrySchema },
-    date:{ type:dateSchema },
-    type:[String],
-    primary_type:String,
-    canonical_url:String,
-    urlid:String,
-    locations:String,
-    states:String
+  id: String,
+  name: String
 });
 
-const Holiday = mongoose.model('Holiday', holidayScehma);
+const dateTimeSchema = new mongoose.Schema({
+  year: Number,
+  month: Number,
+  day: Number,
+  minute: Number,
+  second: Number
+});
+
+const timezoneSchema = new mongoose.Schema({
+  offset: String,
+  zoneabb: String,
+  zoneoffset: Number,
+  zonedst: Number,
+  zonetotaloffset: Number
+});
+
+const dateSchema = new mongoose.Schema({
+  iso: String,
+  datetime: { type: dateTimeSchema },
+  timezone: { type: timezoneSchema }
+});
+
+const holidaySchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  country: { type: countrySchema },
+  date: { type: dateSchema },
+  type: [String],
+  primary_type: String,
+  canonical_url: String,
+  urlid: String,
+  locations: String,
+  states: String
+});
+
+const Holiday = mongoose.model('Holiday', holidaySchema);
 
 export default Holiday;
