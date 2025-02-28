87bcbbbb398f645c163c3728ef3ea6281eb36449
yashilbusa <yashilpatel1914@gmail.com>
2025-02-28 14:31:04 +0530

    commit

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.listen(port,()=>{
 
 app.get("/",(req,res)=>{
     res.writeHead(200,{'Content-Type':'text/html'});
-    res.end("<h1>Hello Yashil Busa From InnovateMR!!!</h1>")
+    res.end("<h1>Hello Yashil Busa From InnovateMR!!!</h1>");
 });
 
 app.use("/",router);
