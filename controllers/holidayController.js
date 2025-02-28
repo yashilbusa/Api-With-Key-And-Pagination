072382951a61170f7813c0281ccf481a089646b1
yashilbusa <yashilpatel1914@gmail.com>
2025-02-28 10:50:04 +0530

    commmit

--- controllers/holidayController.js
+++ controllers/holidayController.js
@@ -40,7 +40,7 @@ const postData = async () => {
   const findAll = async (req, res) => {
     try {
         let page = Number(req.query.page) || 1;
-        let limit = Number(req.query.limit) || 1;
+        let limit = Number(req.query.limit) || 10;
 
         const skip = (page - 1) * limit;
 
